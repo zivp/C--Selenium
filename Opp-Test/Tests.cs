64aca99bc249e3fcf6aa2357d4cd3eaabc912801
Ziv Fahima <zivf@consist.co.il>
2022-01-27 19:35:37 +0200

    init project

---
+++ Opp-Test/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Opp_Test
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        public WebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+           setup st = new setup(Env.Test.ToString(),Browser.Chrome.ToString());
+           driver = st.driver;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+
+        [Test]
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(driver.Title.Contains("Google"));
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Opp-Test/setup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Opp_Test
+{
+
+
+    public class setup
+    {
+        public string browser { get; set; }
+        public string env { get; set; }
+        public WebDriver driver { get; set; }
+
+        public setup(string env, string browser)
+        {
+            this.browser = browser;
+            this.env = env;
+            this.driver = getDriver(browser);
+        }
+
+        protected WebDriver getDriver(string browser)
+        {
+            switch (browser)
+            {
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    return new ChromeDriver();
+                case "IE":
+                    new WebDriverManager.DriverManager().SetUpDriver(new InternetExplorerConfig());
+                    return new InternetExplorerDriver();
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    return new FirefoxDriver();
+                default:
+                    throw new Exception("error ! new driver is not init ...");
+            }
+
+        }
+    }
+
+    enum Browser
+    {
+        Chrome,
+        IE,
+        Firefox
+
+    }
+    enum Env
+    {
+        Test,
+        Prod,
+        Stage
+
+    }
+}
+
+
+   
